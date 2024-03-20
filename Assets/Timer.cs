58ca896e265679eb96082328a38e3b55d7ee9272
uhmiir <ameeraijaz21@gmail.com>
2024-03-19 22:09:47 -0400

    obby text
    text for da obby

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -10,6 +10,10 @@ public class Timer : MonoBehaviour
     public float timeRemaining = 0;
     public bool timeIsRunning = true;
     public TMP_Text timeText;
+    public TMP_Text objective;
+    string spillClean = "Clean All 5 Spills Quick!";
+    string pickup = "Pick Up 4 Mayo Jars!";
+    string odor = "Cleanse 8 BO Clouds!";
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +31,28 @@ public class Timer : MonoBehaviour
                 DisplayTime(timeRemaining);
             }
         }
+
         if(timeRemaining >= 180)
         {
             SceneManager.LoadScene(1);
+        }
+
+        if (timeRemaining >= 0 && timeRemaining <= 60)
+        {
+            objective.text = spillClean;
+            objective.color = Color.yellow;
+        }
+        else if (timeRemaining >= 61 && timeRemaining <= 120)
+        {
+            objective.text = pickup;
+            objective.color = Color.yellow;
+
+        }
+        else if (timeRemaining >= 121 && timeRemaining <= 180)
+        {
+            objective.text = odor;
+            objective.color = Color.yellow;
+
         }
     }
     void DisplayTime (float timeToDisplay)
