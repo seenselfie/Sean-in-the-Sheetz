4c469b56a1773608d6c7d88bc502dba34abb1881
uhmiir <ameeraijaz21@gmail.com>
2024-03-19 20:55:08 -0400

    Update Timer.cs

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -32,6 +32,6 @@ public class Timer : MonoBehaviour
         timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
